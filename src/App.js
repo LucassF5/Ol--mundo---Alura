e672a1e7c482b68116fa21d6ef53e5e29ace1c2a
L F <franco100lucas@gmail.com>
2024-08-07 23:12:46 -0300

    feat: Add Footer and Post component and styles for both of them

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Inicio from "./routes/Inicio"
 import SobreMim from "./routes/SobreMim"
 import Menu from "./components/Menu";
+import Footer from "components/Footer";
 
 function App() {
   return (
@@ -14,6 +15,8 @@ function App() {
         <Route path="/sobremim" element={<SobreMim />} />
         <Route path="*" element={<h1>Página não encontrada</h1>} />
       </Routes>
+
+      <Footer />
     </BrowserRouter>
   );
 }
